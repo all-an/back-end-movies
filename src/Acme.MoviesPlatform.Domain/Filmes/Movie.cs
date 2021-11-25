5a1c63a02c354301c792914bcfe1035a7ac07a5e
all-an <all.ancapistan@gmail.com>
2021-11-25 20:53:17 -0300

    adicionando filme

--- src/Acme.MoviesPlatform.Domain/Filmes/Movie.cs
+++ src/Acme.MoviesPlatform.Domain/Filmes/Movie.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
 
 namespace Acme.MoviesPlatform.Filmes
 {
-    class Movie
+    class Movie : AuditedAggregateRoot<Guid>
     {
+        public string Name { get; set; }
+        
+        public MovieType Type { get; set; }
     }
 }
